5aab065a462e44e5686781abec3054d70b5f237e
motoki0208 <gift0119@gmail.com>
2018-07-19 19:46:42 +0900

    first_commit

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module BigC
+  class Application < Rails::Application
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+      g.stylesheets false
+    end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
