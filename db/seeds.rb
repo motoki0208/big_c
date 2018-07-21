98dfe5ddfd0066f40b31a40f79aaccac5d955632
motoki0208 <gift0119@gmail.com>
2018-07-21 17:05:39 +0900

    add a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+# 各テーブルの値入力用
+CSV.foreach("db/csv/event.csv") do |info|
+  Event.create(name: info[0])
+end
+
+CSV.foreach("db/csv/game.csv") do |info|
+  Game.create(name: info[0])
+end
+
+CSV.foreach("db/csv/lesson.csv") do |info|
+  Lesson.create(name: info[0])
+end
+
+CSV.foreach("db/csv/place.csv") do |info|
+  Place.create(name: info[0])
+end
+
+CSV.foreach("db/csv/program.csv") do |info|
+  Program.create(name: info[0])
+end
+
+CSV.foreach("db/csv/subject.csv") do |info|
+  Subject.create(name: info[0])
+end
+
+CSV.foreach("db/csv/location.csv") do |info|
+  Location.create(name: info[0])
+end
+
+CSV.foreach("db/csv/occupation.csv") do |info|
+  Occupation.create(name: info[0], url: info[1])
+end
