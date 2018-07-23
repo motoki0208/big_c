fb4889313459b25e2e830822898a8c33870a09a7
motoki0208 <gift0119@gmail.com>
2018-07-23 11:54:32 +0900

    seed周りのファイル編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,34 +8,26 @@
 
 require "csv"
 # 各テーブルの値入力用
-CSV.foreach("db/csv/event.csv") do |info|
-  Event.create(name: info[0])
-end
-
-CSV.foreach("db/csv/game.csv") do |info|
-  Game.create(name: info[0])
+CSV.foreach("db/csv/school_event.csv") do |info|
+  SchoolEvent.create(name: info[0])
 end
 
 CSV.foreach("db/csv/lesson.csv") do |info|
   Lesson.create(name: info[0])
 end
 
-CSV.foreach("db/csv/place.csv") do |info|
-  Place.create(name: info[0])
+CSV.foreach("db/csv/school_place.csv") do |info|
+  SchoolPlace.create(name: info[0])
 end
 
-CSV.foreach("db/csv/program.csv") do |info|
-  Program.create(name: info[0])
+CSV.foreach("db/csv/school_subject.csv") do |info|
+  SchoolSubject.create(name: info[0])
 end
 
-CSV.foreach("db/csv/subject.csv") do |info|
-  Subject.create(name: info[0])
-end
-
-CSV.foreach("db/csv/location.csv") do |info|
-  Location.create(name: info[0])
+CSV.foreach("db/csv/hometown.csv") do |info|
+  Hometown.create(name: info[0])
 end
 
 CSV.foreach("db/csv/occupation.csv") do |info|
-  Occupation.create(name: info[0], url: info[1])
+  Occupation.create(name: info[0], url_13hw: info[1])
 end
