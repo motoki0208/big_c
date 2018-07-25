bc6937bf391fa6e8ba5388deec8dd8633e2494fc
hayabusa3703 <umibudou3703@gmail.com>
2018-07-25 20:40:18 +0900

    Likesコントローラー　メソッド定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+  before_action :set_variables
+
+  def like
+    like = current_user.likes.new(story_id: @story.id)
+    like.save
+  end
+
+  def unlike
+    like = current_user.likes.find_by(story_id: @story.id)
+    like.destroy
+  end
+
+  private
+
+  def set_variables
+    @story = Story.find(params[:story_id])
+    @id_name = "#like-link-#{@story.id}"
+  end
+
 end
