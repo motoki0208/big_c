b84ff87055446f65813d36d414cc9697a4ab6aa0
白柳明香里 <asm13.5taykox@gmail.com>
2018-07-27 01:20:50 +0900

    スクロールヘッダ完成

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,12 +1,16 @@
 $(window).bind("load", function(){
-    if(document.URL.match("stars/" + "[0-9０-９]")) {
-
+    if(document.URL.match("addictions/" + "[0-9０-９]")) {
       $(function() {
           //スクロールスパイ
-          $("#header_show a").click(function(){
-            var id = $(this).attr("href");
-            var position = $(id).get( 0 ).offsetTop - 70
-            $('.contents').animate({scrollTop: position}, 500)
+          $("#header_show li a").click(function(){
+            var speed = 500;
+            var href = $(this).attr("href");
+            console.log(href);
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top - 115;
+            console.log(position);
+            $('html,body').animate({scrollTop: position}, speed, "swing");
+            return false;
           });
       });
     };
