f3ba2319b6b4ac884f543917e89d1262363693be
白柳明香里 <asm13.5taykox@gmail.com>
2018-07-27 11:08:48 +0900

    田畑氏レビュー箇所修正

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -5,10 +5,8 @@ $(window).bind("load", function(){
           $("#header_show li a").click(function(){
             var speed = 500;
             var href = $(this).attr("href");
-            console.log(href);
             var target = $(href == "#" || href == "" ? 'html' : href);
             var position = target.offset().top - 120;
-            console.log(position);
             $('html,body').animate({scrollTop: position}, speed, "swing");
             return false;
           });
