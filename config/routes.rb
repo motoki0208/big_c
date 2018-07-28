b872b6d8a050ea788b762ac9234a91b56865a8a7
motoki0208 <gift0119@gmail.com>
2018-07-28 22:22:21 +0900

    ルートパス変更及びコントローラのアクション設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'stars#index'
+  root 'addictions#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   resources :stars, only: [:index, :show]
   resources :addictions, only: [:index, :show]
 end
