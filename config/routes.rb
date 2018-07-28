710a530cbd846324681f4fb55dd8bf8e784c650f
hayabusa3703 <umibudou3703@gmail.com>
2018-07-28 11:50:21 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'stars#index'
+  post   '/like/:addiction_id' => 'likes#like',   as: 'like'
+  delete '/like/:addiction_id' => 'likes#unlike', as: 'unlike'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
