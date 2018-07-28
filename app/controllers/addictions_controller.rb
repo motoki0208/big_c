2f2694e11522d613416eab04342b2347ef350a46
hayabusa3703 <umibudou3703@gmail.com>
2018-07-28 12:19:27 +0900

    addictionのindex画面にてハート機能の実装

--- app/controllers/addictions_controller.rb
+++ app/controllers/addictions_controller.rb
@@ -3,6 +3,7 @@ class AddictionsController < ApplicationController
   def index
     @q = Star.ransack(params[:q])
     @stars = @q.result(distinct: true)
+    @addictions = Addiction.all
   end
   
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   before_action :set_variables
 
   def like
-    like = current_user.likes.new(addcition_id: @addcition.id)
+    like = current_user.likes.new(addiction_id: @addiction.id)
     like.save
   end
 
   def unlike
-    like = current_user.likes.find_by(addcition_id: @addcition.id)
+    like = current_user.likes.find_by(addiction_id: @addiction.id)
     like.destroy
   end
 
@@ -15,7 +15,7 @@ class LikesController < ApplicationController
 
   def set_variables
     @addiction = Addiction.find(params[:addiction_id])
-    @id_name = "#like-link-#{@addcition.id}"
+    @id_name = "#like-link-#{@addiction.id}"
   end
 
 end
