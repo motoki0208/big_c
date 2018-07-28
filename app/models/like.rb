ec3569a5f196395e74479f16add2aa2a120203e1
hayabusa3703 <umibudou3703@gmail.com>
2018-07-28 11:46:54 +0900

    アソシエーションの定義

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,5 @@
 class Like < ApplicationRecord
+
+  belongs_to :addiction, counter_cache: :likes_count
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :addictions
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :likes, dependent: :destroy
+  has_many :like_addictions, through: :likes, source: :addictions
+
 end
