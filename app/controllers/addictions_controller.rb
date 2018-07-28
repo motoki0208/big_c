beb06d9e899246ef36a44cb7e5b9f41e2d73de04
hayabusa3703 <umibudou3703@gmail.com>
2018-07-28 12:49:05 +0900

    Show画面にてハート機能の実装

--- app/controllers/addictions_controller.rb
+++ app/controllers/addictions_controller.rb
@@ -1,9 +1,18 @@
 class AddictionsController < ApplicationController
 
+before_action :set_addiction, only: [:show]
+
   def index
     @q = Star.ransack(params[:q])
     @stars = @q.result(distinct: true)
     @addictions = Addiction.all
   end
-  
+
+  def show
+  end
+
+  private
+    def set_addiction
+      @addiction = Addiction.find(params[:id])
+    end
 end
