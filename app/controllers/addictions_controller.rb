9c07c1c3ff981dfcd5935b522348f1480d6349c3
hayabusa3703 <umibudou3703@gmail.com>
2018-07-29 16:42:23 +0900

    like_detail関連削除

--- app/controllers/addictions_controller.rb
+++ app/controllers/addictions_controller.rb
@@ -11,10 +11,6 @@ before_action :set_addiction, only: [:show]
   def show
   end
 
-  def like_detail
-    @addictions = Addiction.all
-  end
-
   private
     def set_addiction
       @addiction = Addiction.find(params[:id])
