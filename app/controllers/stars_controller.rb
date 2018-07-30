a56109e6b99a87804f4c26dbddd0bf4ff640c070
白柳明香里 <asm13.5taykox@gmail.com>
2018-07-30 16:17:22 +0900

    stars_controllerのsyntax error解消

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,5 +1,5 @@
 class StarsController < ApplicationController
-  before_action :set_star, only: [:show, :edit, :update, :destroy]
+  before_action :set_star, only: [:show]
 
   # GET /stars
   # GET /stars.json
@@ -13,6 +13,7 @@ class StarsController < ApplicationController
           @tags = WorryTag.where( 'text Like(?)',"%#{params[:keyword]}%" )
         end
       }
+    end
   end
 
   # GET /stars/1
@@ -20,54 +21,54 @@ class StarsController < ApplicationController
   def show
   end
 
-  # GET /stars/new
-  def new
-    @star = Star.new
-  end
+  # # GET /stars/new
+  # def new
+  #   @star = Star.new
+  # end
 
-  # GET /stars/1/edit
-  def edit
-  end
+  # # GET /stars/1/edit
+  # def edit
+  # end
 
-  # POST /stars
-  # POST /stars.json
-  def create
-    @star = Star.new(star_params)
+  # # POST /stars
+  # # POST /stars.json
+  # def create
+  #   @star = Star.new(star_params)
 
-    respond_to do |format|
-      if @star.save
-        format.html { redirect_to @star, notice: 'Star was successfully created.' }
-        format.json { render :show, status: :created, location: @star }
-      else
-        format.html { render :new }
-        format.json { render json: @star.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @star.save
+  #       format.html { redirect_to @star, notice: 'Star was successfully created.' }
+  #       format.json { render :show, status: :created, location: @star }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @star.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # PATCH/PUT /stars/1
-  # PATCH/PUT /stars/1.json
-  def update
-    respond_to do |format|
-      if @star.update(star_params)
-        format.html { redirect_to @star, notice: 'Star was successfully updated.' }
-        format.json { render :show, status: :ok, location: @star }
-      else
-        format.html { render :edit }
-        format.json { render json: @star.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /stars/1
+  # # PATCH/PUT /stars/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @star.update(star_params)
+  #       format.html { redirect_to @star, notice: 'Star was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @star }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @star.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /stars/1
-  # DELETE /stars/1.json
-  def destroy
-    @star.destroy
-    respond_to do |format|
-      format.html { redirect_to stars_url, notice: 'Star was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /stars/1
+  # # DELETE /stars/1.json
+  # def destroy
+  #   @star.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to stars_url, notice: 'Star was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
