2b395518541620030a3ea8f797d2fd093bf354ed
白柳明香里 <asm13.5taykox@gmail.com>
2018-07-30 19:09:26 +0900

    ログアウト機能にconfirm設定

--- app/controllers/addictions_controller.rb
+++ app/controllers/addictions_controller.rb
@@ -1,26 +1,27 @@
 class AddictionsController < ApplicationController
   before_action :set_addiction, only: :show
+  before_action :set_ransak_q,  only: [:index, :show, :search]
 
   def index
-    @q = Star.ransack(params[:q])
-    @stars = @q.result(distinct: true)
-
-    @addictions_job  = Addiction.job.rand10.includes(:star)
-    @addictions_hobby  = Addiction.hobby.rand10.includes(:star)
+    @addictions_job     = Addiction.job.rand10.includes(:star)
+    @addictions_hobby   = Addiction.hobby.rand10.includes(:star)
     @addictions_browsed = Addiction.rand10.includes(:star)
   end
 
   def show
-   @star = @addiction.star
+    @star = @addiction.star
   end
   
   def search
-    @q = Star.ransack(params[:q])
-    @stars = @q.result(distinct: true)
   end
 
   private
   def set_addiction
     @addiction = Addiction.find(params[:id])
+  end
+
+  def set_ransak_q
+    @q     = Star.ransack(params[:q])
+    @stars = @q.result(distinct: true)
   end
 end
